57212338d111b06c7300188c50b7dc2e9fa89cd1
Hyecheol (Jerry) Jang <hyecheol.jang@wisc.edu>
2019-08-20 00:35:15 +0900

    Implementation of bash script and configuration file + Update Acknowledgement and README
    Getting current ip address of server, check whether it is same with current DNS record's IP, and update to the DNS server if it is different.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Cloudflare-Public-Dynamic-IP-Update
-Updating public dynamic IP address to existing cloudflare DNS record
+Updating public dynamic IP address to existing cloudflare DNS record (Only work for updating single DNS Zone)
+
+
+## Requirement
+Using [curl](https://en.wikipedia.org/wiki/CURL), [jq](https://stedolan.github.io/jq/), and [dig](https://en.wikipedia.org/wiki/Dig_(command)) command. <br/>
+Check whether jq is installed on your system or not before use this script
+
+Need to customize cloudflare_config file before using this script.
+- **Auth-Key**<br>
+Authorization key for cloudflare API. Able to find it at **My Profile** page. Substitute **[Your_CloudFlare_API_Auth_Key]** with your API key.<br>
+*Follow this [Instruction](https://support.cloudflare.com/hc/en-us/articles/200167836-Where-do-I-find-my-Cloudflare-API-key-)*
+- **Auth-Email**<br>
+Your Cloudflare account login email. Substitute **[Your_CloudFlare_Account_Email]** with yours.
+- **Zone-ID**<br>
+You can find zone ID of your DNS Zone on the right sidebar of cloudflare DNS overview page, under API tab. Substitute **[DNS_Zone_ID_of_Your_Domain]** with yours.
+- **Update-Target**<br>
+Meaning list of domain address that you want to update linking ip address via this script. List must be separated by single comma(,). Substitute **[List_Of_Domain_Address_You_Want_To_Update]** with your list.<br>
+*e.g. "google.com,www.google.com"*
+
+
+## Tested Environment
+If you used this code in other OS and confirm that it works successfully (in all situation), please make an issue with brief test result.<br/>
+As soon as I check the issue, I will update the list.
+
+#### Test Condition
+- Current IP is same as record's IP, must see the proper message indicating the identity
+- Current IP is not same as record's IP, record's IP must be updated and see the proper message indicating the record has been updated
+- Has no connection, and see the error message
+
+#### Tested OS List
+- Ubuntu 18.04.2 LTS
+
+
+## Acknowledgement
+This work is a part of website rebuilding project, supported by KSEA-Wisconsin, Korean-American Scientists and Engineers Association - Wisconsin Chapter.
+
+## Contact
+If you have any question or want to report an error (or any other suggestion), simply make an issue with description.
+
+If you want DNS record IP update shell script for multiple DNS zones, I may work for that. Just simple post an issue on this github repository.
---
+++ cloudflare_dynamic_ip_update.bash
@@ -0,0 +1,119 @@
+#/usr/bin/env bash
+
+## Author: Hyecheol (Jerry) Jang
+## Shell Script that check current public (dynamic) ip address of server,
+## and update it to the Cloudflare DNS record after comparing ip address registered to Cloudflare
+## basic shell scripting guide https://blog.gaerae.com/2015/01/bash-hello-world.html
+
+## Using dig command (https://en.wikipedia.org/wiki/Dig_(command)) to get current public IP address
+currentIP=$(dig -4 TXT +short o-o.myaddr.1.google.com @ns1.google.com)
+if [ $? == 0 ] && [ ${currentIP} ]; then  ## when dig command run without error,
+    ## Making substring, only retrieving ip address of this server
+    ## https://stackabuse.com/substrings-in-bash/
+    currentIP=$(echo $currentIP | cut -d'"' -f 2)
+    echo "current public IP address is "$currentIP
+else  ## error happens,
+    echo "Check your internet connection, or google DNS server maybe interruptted"
+    exit
+fi
+
+## Use Cloudflare API to retrieve recordIP
+## https://api.cloudflare.com/
+## Read configuration from separated cloudflare_config file (need to locate in the same directory)
+## https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
+## https://stackoverflow.com/questions/10586153/split-string-into-an-array-in-bash
+readResult=""  ## Store configuration
+while IFS= read -r line || [[ -n "$line" ]]; do
+    readResult+=" "  ## Delimiter: space
+    readResult+="$line"
+done < cloudflare_config  ## use cloudflare_config file
+unset IFS
+
+## After retrieve information from file, cut result string into configuration elements
+key=$(echo $(echo $readResult | cut -d' ' -f 1) | cut -d'=' -f 2)
+email=$(echo $(echo $readResult | cut -d' ' -f 2) | cut -d'=' -f 2)
+zoneID=$(echo $(echo $readResult | cut -d' ' -f 3) | cut -d'=' -f 2)
+IFS=',' read -r -a updateTarget <<< "$(echo $(echo $readResult | cut -d' ' -f 4) | cut -d'=' -f 2)"
+unset readResult
+unset IFS
+
+## Make space for saving record's IP Address, Type, and Name
+declare -a recordIP
+declare -a recordType
+declare -a recordName
+declare -a dnsID
+declare -a recordProxied
+for string in ${updateTarget[@]}; do  ## retrieve record's IP Address and save to recordIP
+    content=$(
+        curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneID/dns_records?name=${string}" \
+            -H "X-Auth-Email: $email" \
+            -H "X-Auth-Key: $key" \
+            -H "Content-Type: application/json"
+    )
+    ## Parse JSON  https://stackoverflow.com/questions/42427371/cloudflare-api-cut-json-response
+    ## Using jq  https://stedolan.github.io/jq/
+    ip=$(echo $content | jq '.result | map(.content) | add' | cut -d'"' -f 2)
+    rType=$(echo $content | jq '.result | map(.type) | add' | cut -d'"' -f 2)
+    name=$(echo $content | jq '.result | map(.name) | add' | cut -d'"' -f 2)
+    id=$(echo $content | jq '.result | map(.id) | add' | cut -d'"' -f 2)
+    proxied=$(echo $content | jq '.result | map(.proxied) | add' | cut -d'"' -f 2)
+    recordIP=(${recordIP[@]} $ip)
+    recordType=(${recordType[@]} $rType)
+    recordName=(${recordName[@]} $name)
+    dnsID=(${dnsID[@]} $id)
+    recordProxied=(${recordProxied[@]} $proxied)
+    unset id
+    unset rType
+    unset name
+    unset ip
+    unset content
+    unset string
+    unset proxied
+done
+unset updateTarget
+
+## Compare currentIP and recordIP
+declare -a needUpdate  ## Array to store whether each record needs to be updated or not
+for string in ${recordIP[@]}; do
+    if [ ${string} == ${currentIP} ]; then
+        needUpdate=(${needUpdate[@]} 'False')
+    else
+        needUpdate=(${needUpdate[@]} 'True')
+    fi
+    unset string
+done
+unset recordIP  ## X Need recordIP Anymore
+
+## Update record if needed
+count=0
+while [ $count -lt ${#needUpdate[@]} ]; do
+    if [ ${needUpdate[count]} == 'True' ]; then
+        echo "record IP needs to be updated for "${recordName[count]}
+        success=$(
+            curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneID/dns_records/${dnsID[count]}" \
+                -H "X-Auth-Email: $email" \
+                -H "X-Auth-Key: $key" \
+                -H "Content-Type: application/json" \
+                --data '{"type":"'"${recordType[count]}"'","name":"'"${recordName[count]}"'","content":"'"$currentIP"'","proxied":'${recordProxied[count]}'}' | \
+            jq '.success' | cut -d'"' -f 2
+        )
+        if [ $success == true ]; then
+            echo "Success update record IP of "${recordName[count]}
+        else
+            echo "Fail to update record IP of "${recordName[count]}"\n""Please Check result!!"
+        fi
+    else
+        echo "record IP does not need to be updated for "${recordName[count]}
+    fi
+    count=$((${count}+1))
+done
+unset count
+unset currentIP
+unset key
+unset email
+unset zoneID
+unset recordType
+unset recordName
+unset dnsID
+unset recordProxied
+unset needUpdate
