ca0aa1d452e6a8aa2af1168c74f5d6d8bd1f6799
Hyecheol (Jerry) Jang <hyecheol.jang@wisc.edu>
2019-08-21 18:14:01 +0900

    fix config file location (to absolute path)
    crontab does not care about file location of script. need to use absolute path

--- cloudflare_dynamic_ip_update.bash
+++ cloudflare_dynamic_ip_update.bash
@@ -22,11 +22,12 @@ fi
 ## Read configuration from separated cloudflare_config file (need to locate in the same directory)
 ## https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
 ## https://stackoverflow.com/questions/10586153/split-string-into-an-array-in-bash
+SCRIPT_PATH=SCRIPT_PATH=$(dirname $(realpath $0))
 readResult=""  ## Store configuration
 while IFS= read -r line || [[ -n "$line" ]]; do
     readResult+=" "  ## Delimiter: space
     readResult+="$line"
-done < cloudflare_config  ## use cloudflare_config file
+done < $SCRIPT_PATH"/cloudflare_config"  ## use cloudflare_config file
 unset IFS
 
 ## After retrieve information from file, cut result string into configuration elements
